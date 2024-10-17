643c71a3a29b84083efefa3b9012618a6f32039c
Mowaffak Alraiyes <mo.alraijr@gmail.com>
2024-10-17 16:05:40 -0500

    Initial Commit

---
+++ McatMaestro/McatMaestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  McatMaestroApp.swift
+//  McatMaestro
+//
+//  Created by Mo Alraiyes on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct McatMaestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ McatMaestroTests/McatMaestroTests.swift
@@ -0,0 +1,17 @@
+//
+//  McatMaestroTests.swift
+//  McatMaestroTests
+//
+//  Created by Mo Alraiyes on 10/17/24.
+//
+
+import Testing
+@testable import McatMaestro
+
+struct McatMaestroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
